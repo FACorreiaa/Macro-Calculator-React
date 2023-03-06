2be89a9086fe54f756efe6fe30090ec1de58278f
fc <fc@gsmk.de>
2023-03-06 16:47:39 +0100

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	mode: 'jit',
+	content: ['./index.html', './src/**/*.{html,ts,tsx}', './src/*.{ts,tsx}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [
+		require('@tailwindcss/forms'),
+
 		require('@catppuccin/tailwindcss')({
-			// prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
-			// default is `false`, which means no prefix
-			prefix: 'ctp',
-			// which flavour of colours to use by default, in the `:root`
 			defaultFlavour: 'frappe',
 		}),
 	],
