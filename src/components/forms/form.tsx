da86cdbe0c0a4de4812d1f46b2a3d6566216c104
fc <fc@gsmk.de>
2023-03-06 22:50:27 +0100

    change button style

--- src/components/forms/form.tsx
+++ src/components/forms/form.tsx
@@ -187,7 +187,7 @@ function CustomForm() {
 						<button
 							disabled
 							type="button"
-							className="text-white disabled:bg-slate-300 disabled:cursor-not-allowed	 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+							className="text-white disabled:bg-slate-300 disabled:cursor-not-allowed	 bg-gray-800 hover:bg-gray-900 disabled:hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 disabled:dark:hover:bg-gray-200 dark:focus:ring-gray-700 dark:border-gray-700">
 							Prev
 						</button>
 						<button
