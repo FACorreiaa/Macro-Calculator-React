cb8f8c2904a3e4b296ae5bc27a68ec084401eb32
fc <fc@gsmk.de>
2023-03-07 19:24:45 +0100

    creation of custom components

--- src/components/forms/form.tsx
+++ src/components/forms/form.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useZodForm from '../../hooks/useZodForm';
 import { tdeeSchema } from '../../types/tdeeSchema';
+import CustomSelect from '../select';
 import styles from '../styles/forms.module.css';
 type RegisterPageProps = {
 	age: number;
@@ -8,6 +9,7 @@ type RegisterPageProps = {
 	weight: number;
 };
 
+const measureValues = ['Metric', 'Imperial'];
 function CustomForm() {
 	const methods = useZodForm({
 		schema: tdeeSchema,
@@ -31,32 +33,17 @@ function CustomForm() {
 		<React.Fragment>
 			<div className="w-full max-w-xs ">
 				<form
-					className=" shadow-md rounded p-8 mb-4 bg-gray-200 dark:bg-slate-500"
+					className="shadow-lg rounded-xl p-8 mb-4 bg-gray-200 dark:bg-slate-500"
 					onSubmit={methods.handleSubmit(onSumitRegisterValues)}>
 					<span className="text-gray-700 text-xxl font-bold mb-2 text-center w-full">
 						Calculate your TDEE
 					</span>
-					<div className="mb-2">
-						<label className="block text-gray-200 dark:text-gray-900 text-sm font-bold mb-2">
-							Select your metric
-						</label>
-						<div className="inline-block relative w-64">
-							<select
-								id="metrics"
-								className="dark:text-slate-900 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-								<option selected>Metric</option>
-								<option>Imperial</option>
-							</select>
-							<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-								<svg
-									className="fill-current h-4 w-4"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 20 20">
-									<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-								</svg>
-							</div>
-						</div>
-					</div>
+					<CustomSelect
+						label="Select your metric"
+						id="metrics"
+						options={measureValues}
+						selected
+					/>
 					<div className="mb-2">
 						<label className="block text-gray-200 dark:text-gray-900 text-sm font-bold mb-2">
 							Age
@@ -182,16 +169,16 @@ function CustomForm() {
 						</div>
 					</div>
 
-					<div className="flex justify-between mb-2">
+					<div className="flex justify-between">
 						<button
 							disabled
 							type="button"
-							className="text-white disabled:bg-slate-300 disabled:cursor-not-allowed	 bg-gray-800 hover:bg-gray-900 disabled:hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 disabled:dark:hover:bg-gray-200 dark:focus:ring-gray-700 dark:border-gray-700">
+							className="text-white disabled:bg-slate-300 disabled:cursor-not-allowed	 bg-gray-800 hover:bg-gray-900 disabled:hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 disabled:dark:hover:bg-gray-200 disabled:text-slate-900 font-bold dark:focus:ring-gray-700 dark:border-gray-700">
 							Prev
 						</button>
 						<button
 							type="button"
-							className="text-white disabled:bg-slate-300 disabled:cursor-not-allowed bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+							className="text-white disabled:bg-slate-300 disabled:cursor-not-allowed bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-bold rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
 							Next
 						</button>
 					</div>
--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
-
-function CustomSelect() {
-	return <div>CustomSelect</div>;
+type CustomSelectProps = {
+	label: string;
+	id: string;
+	options: string[];
+	selected?: boolean;
+};
+function CustomSelect({ label, id, selected, options }: CustomSelectProps) {
+	return (
+		<div className="mb-2">
+			<label className="block text-gray-200 dark:text-gray-900 text-sm font-bold mb-2">
+				{label}
+			</label>
+			<div className="inline-block relative w-64">
+				<select
+					id={id}
+					className="dark:text-slate-900 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+					{options.map((option: string) => {
+						return <option selected={selected}>{option}</option>;
+					})}
+				</select>
+				<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+					<svg
+						className="fill-current h-4 w-4"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20">
+						<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+					</svg>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CustomSelect;
