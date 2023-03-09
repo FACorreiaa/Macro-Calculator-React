564da59dd2959e5d137cb55f32a5355f50ac730f
fc <fc@gsmk.de>
2023-03-09 14:08:39 +0100

    add more pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import GoalPage from './pages/goal';
+import ResultsPage from './pages/results';
 import TdeePage from './pages/tdee';
+import './index.css';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <TdeePage />,
+	},
+	{
+		path: '/goals',
+		element: <GoalPage />,
+	},
+	{
+		path: '/results',
+		element: <ResultsPage />,
 	},
 ]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
