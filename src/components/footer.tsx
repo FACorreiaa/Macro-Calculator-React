76a8dc1dead32bc4daa2338b64a51db5d850376f
fc <fc@gsmk.de>
2023-03-13 14:59:13 +0100

    update style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FooterComponent() {
 	return (
-		<p className="text-center text-stone-200 text-xs">
+		<p className="text-center text-stone-200 text-sm font-bold ">
 			&copy;2020 Acme Corp. All rights reserved.
 		</p>
 	);
