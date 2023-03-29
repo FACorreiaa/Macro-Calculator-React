59a99bb9eec5cd1382cecf4083814b7434614f5e
fc <fc@gsmk.de>
2023-03-29 11:05:08 +0200

    deleted unused code

--- src/components/dashboard/macro-table.tsx
+++ src/components/dashboard/macro-table.tsx
@@ -1,5 +1,7 @@
+import { DietPlan } from '../../helper/data';
+
 type MacroTableProps = {
-	macroDistribution: any;
+	macroDistribution: DietPlan;
 };
 
 function MacroTable({ macroDistribution }: MacroTableProps) {
@@ -41,36 +43,6 @@ function MacroTable({ macroDistribution }: MacroTableProps) {
 							</tr>
 						);
 					})}
-					{/* <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-						<th
-							scope="row"
-							className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-							High Carb
-						</th>
-						<td className="px-6 py-4">x</td>
-						<td className="px-6 py-4">y</td>
-						<td className="px-6 py-4">$z</td>
-					</tr>
-					<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-						<th
-							scope="row"
-							className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-							Low Carb
-						</th>
-						<td className="px-6 py-4">1</td>
-						<td className="px-6 py-4">2</td>
-						<td className="px-6 py-4">3</td>
-					</tr>
-					<tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-						<th
-							scope="row"
-							className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-							Moderate Carb
-						</th>
-						<td className="px-6 py-4">a</td>
-						<td className="px-6 py-4">b</td>
-						<td className="px-6 py-4">$c</td>
-					</tr> */}
 				</tbody>
 			</table>
 		</div>
