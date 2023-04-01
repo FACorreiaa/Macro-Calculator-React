4d9f31b87ef5a610a3635005b87c2524b0d41f9b
fc <fc@gsmk.de>
2023-04-01 23:28:48 +0200

    change layout

--- src/components/dashboard/main-info.tsx
+++ src/components/dashboard/main-info.tsx
@@ -22,73 +22,75 @@ export const DisplayBaseInfo = () => {
 	const metric = bmrData.metric;
 	return (
 		<div className="bg-slate-100 border border-slate-400  p-2  rounded relative text-center m-5">
-			<div className="justify-between flex flex-row p-4 m-5">
-				<div className="w-64 shadow-sm justify-center  shadow-slate-800  bg-white p-6 border border-spacing-2 rounded">
-					<h1 className="text-xl font-bold mb-2">Biometric Data</h1>
-					<p className="text-sm">
-						<label className="font-bold">Sex: </label>
-						<span>{bmrData.sex}</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">Age: </label>
-						<span>{bmrData.age}</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">Weight: </label>
-						<span>
-							{bmrData.weight} {Measure[metric].weight}
-						</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">Height: </label>
-						<span>
-							{bmrData.height} {Measure[metric].height}
-						</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">BMR: </label>
-						<span>
-							{bmr} {KCALS}
-						</span>
-					</p>
-				</div>
-				<div className="w-64 shadow-sm justify-center  shadow-slate-800  bg-white p-6 border border-spacing-2 rounded">
-					<h1 className="text-xl font-bold mb-2">TDEE</h1>
-					<p className="text-sm ">
-						<span className="text-lg">
-							{tdee} {KCALS}
-						</span>
-					</p>
-				</div>
-				<div className="w-64 shadow-sm justify-center  shadow-slate-800  bg-white p-6 border border-spacing-2 rounded">
-					<h1 className="text-xl font-bold mb-2">Goals and Objectives</h1>
+			<div className="p-4 m-5">
+				<div className="w-full shadow-sm justify-between flex flex-row sm:flex-column sm:justify-center shadow-slate-800  bg-white p-6 border border-spacing-2 rounded">
+					<div>
+						<h1 className="text-xl font-bold mb-2">Biometric Data</h1>
+						<p className="text-sm">
+							<label className="font-bold">Sex: </label>
+							<span>{bmrData.sex}</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">Age: </label>
+							<span>{bmrData.age}</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">Weight: </label>
+							<span>
+								{bmrData.weight} {Measure[metric].weight}
+							</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">Height: </label>
+							<span>
+								{bmrData.height} {Measure[metric].height}
+							</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">BMR: </label>
+							<span>
+								{bmr} {KCALS}
+							</span>
+						</p>
+					</div>
+					<div>
+						<h1 className="text-xl font-bold mb-2">TDEE</h1>
+						<p className="text-sm ">
+							<span className="text-lg">
+								{tdee} {KCALS}
+							</span>
+						</p>
+					</div>
+					<div>
+						<h1 className="text-xl font-bold mb-2">Goals and Objectives</h1>
 
-					<p className="text-sm">
-						<label className="font-bold">Objective: </label>
-						<span>{objective}</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">Activity: </label>
-						<span>{activity}</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">Maintenance calories: </label>
-						<span>
-							{allDietObjectives.Maintenance} {KCALS}
-						</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">Bulking Calories: </label>
-						<span>
-							{allDietObjectives.Bulking} {KCALS}
-						</span>
-					</p>
-					<p className="text-sm">
-						<label className="font-bold">Cutting Calories: </label>
-						<span>
-							{allDietObjectives.Cutting} {KCALS}
-						</span>
-					</p>
+						<p className="text-sm">
+							<label className="font-bold">Objective: </label>
+							<span>{objective}</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">Activity: </label>
+							<span>{activity}</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">Maintenance calories: </label>
+							<span>
+								{allDietObjectives.Maintenance} {KCALS}
+							</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">Bulking Calories: </label>
+							<span>
+								{allDietObjectives.Bulking} {KCALS}
+							</span>
+						</p>
+						<p className="text-sm">
+							<label className="font-bold">Cutting Calories: </label>
+							<span>
+								{allDietObjectives.Cutting} {KCALS}
+							</span>
+						</p>
+					</div>
 				</div>
 			</div>
 			<div className="justify-between flex flex-row p-4 m-5">
